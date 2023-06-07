a1bc73d6bbcf843008f4230c104eeea7e969cf10
Abdur Rahman <abdurrahman27485@gmail.com>
2023-06-07 15:28:57 +0600

    'initialized'

---
+++ src/layouts/Primary.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Primary = () => {
+    return (
+        <div>
+            <h2>Project Initialized</h2>
+        </div>
+    );
+};
+
+export default Primary;
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Primary from './layouts/primary'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Primary></Primary>
+  </React.StrictMode>,
+)
