a614464bcda48a8b4d308d34d450b4a304ed167d
Abdur Rahman <abdurrahman27485@gmail.com>
2023-06-17 20:55:14 +0600

    icons to dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,10 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../providers/AuthProvider';
 import { NavLink, Outlet, json } from 'react-router-dom';
-import { HiUserGroup } from 'react-icons/hi'
-import { SiGoogleclassroom } from 'react-icons/si'
+import { HiUserGroup } from 'react-icons/hi';
+import { SiGoogleclassroom } from 'react-icons/si';
+import { IoMdAddCircle } from 'react-icons/io';
+import {MdPlayLesson} from 'react-icons/md'
 
 const Dashboard = () => {
     const { user } = useContext(AuthContext);
@@ -28,10 +30,10 @@ const Dashboard = () => {
                             <div className='col-span-12 lg:col-span-3 bg-base-200'>
                                 <ul className='flex lg:block justify-around'>
                                     <li className='w-full'>
-                                        <NavLink className="w-full hover:bg-base-300 transition duration-100 px-4 py-2 font-semibold lg:text-2xl flex gap-2 items-center" to={'manageClasses'}><SiGoogleclassroom className="h-6 w-6 lg:h-10 lg:w-10"></SiGoogleclassroom> Manage Classes</NavLink>
+                                        <NavLink className="w-full hover:bg-base-300 transition duration-100 px-4 py-2 font-semibold lg:text-2xl flex gap-2 items-center" to={'manageClasses'}><SiGoogleclassroom className="h-6 w-6 lg:h-8 lg:w-8"></SiGoogleclassroom> Manage Classes</NavLink>
                                     </li>
                                     <li className='w-full'>
-                                        <NavLink className="w-full hover:bg-base-300 transition duration-100 px-4 py-2 font-semibold lg:text-2xl flex gap-2 items-center" to={'manageUsers'}><HiUserGroup className="h-6 w-6 lg:h-10 lg:w-10"></HiUserGroup> Manage Users</NavLink>
+                                        <NavLink className="w-full hover:bg-base-300 transition duration-100 px-4 py-2 font-semibold lg:text-2xl flex gap-2 items-center" to={'manageUsers'}><HiUserGroup className="h-6 w-6 lg:h-8 lg:w-8"></HiUserGroup> Manage Users</NavLink>
                                     </li>
                                 </ul>
 
@@ -66,10 +68,10 @@ const Dashboard = () => {
                             <div className='col-span-12 lg:col-span-3 bg-base-200'>
                                 <ul className='flex lg:block justify-around'>
                                     <li className='w-full'>
-                                        <NavLink className="w-full hover:bg-base-300 transition duration-100 block px-4 py-2 font-semibold lg:text-2xl" to={'manageClasses'}>Add Class</NavLink>
+                                        <NavLink className="w-full hover:bg-base-300 transition duration-100 px-4 py-2 font-semibold lg:text-2xl flex gap-2 items-center" to={'addClass'}><IoMdAddCircle className="h-6 w-6 lg:h-8 lg:w-8"></IoMdAddCircle> Add Class</NavLink>
                                     </li>
                                     <li className='w-full'>
-                                        <NavLink className="w-full hover:bg-base-300 transition duration-100 block px-4 py-2 font-semibold lg:text-2xl" to={'manageUsers'}>My Classes</NavLink>
+                                        <NavLink className="w-full hover:bg-base-300 transition duration-100 px-4 py-2 font-semibold lg:text-2xl flex gap-2 items-center" to={'myClasses'}><MdPlayLesson className="h-6 w-6 lg:h-8 lg:w-8"></MdPlayLesson> My Classes</NavLink>
                                     </li>
                                 </ul>
 
